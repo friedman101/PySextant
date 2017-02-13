69ae5c5c837bd4eee7c9eb3ed84ed95bc246c8aa
David <david@dapbook.local>
2017-02-12 22:15:12 -0800

    add beijing to test utility

--- test.py
+++ test.py
@@ -16,3 +16,16 @@ posix_2 = 1486923600
 print('Seatle lat = %0.2f, lon = %0.2f' % (47.0+36.0/60, -122.0-19.0/60))
 print('Est 1 lat = %0.2f, lon = %0.2f' % (ll1[0,0]*180/pi, ll1[1,0]*180/pi))
 print('Est 2 lat = %0.2f, lon = %0.2f' % (ll2[0,0]*180/pi, ll2[1,0]*180/pi))
+
+# sun elevation at feb 12 2017 13:20:00 in beijing
+theta1 = 35.16*pi/180
+posix_1 = 1486876800
+# sun elevation at feb 12 2017 10:20:00 in beijing 
+theta2 = 28.67*pi/180
+posix_2 = 1486866000
+
+(ll1, ll2) = est_lla(theta1, theta2, posix_1, posix_2)
+
+print('Beijing lat = %0.2f, lon = %0.2f' % (39+55.0/60, +116.0+25.0/60))
+print('Est 1 lat = %0.2f, lon = %0.2f' % (ll1[0,0]*180/pi, ll1[1,0]*180/pi))
+print('Est 2 lat = %0.2f, lon = %0.2f' % (ll2[0,0]*180/pi, ll2[1,0]*180/pi))
